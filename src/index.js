9c56d9e30a7c12faa64439b7ba927eefd47cfcf5
cztamas <tamas.czank@gmail.com>
2018-06-14 19:53:51 +0200

    config handling + connect to db

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+"use strict";
+
 const express = require("express");
+const nconf = require("nconf").argv().env().file("./config.json");
 const path = require("path");
+
+const configFilePath = path.join(__dirname, "config.json");
+nconf.argv().env().file(configFilePath);
+
+console.log("beer", nconf.get("DB_URL"));
 const dataRouter = require("./server");
 
 const app = express();
-const defaultPort = 5000;
-
-const port = process.env.PORT || defaultPort;
+const port = nconf.get("PORT");
 
 app.use("/", express.static(path.join(__dirname, "client")));
 app.use("/data", dataRouter);
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,43 @@
+"use strict";
+
 const express = require("express");
+const mongodb = require("mongodb");
+const nconf = require("nconf");
 
 const router = express.Router();
+const MongoClient = mongodb.MongoClient;
+
+const dbUser = nconf.get("DB_USERNAME");
+const dbPassword = nconf.get("DB_PASSWORD");
+const dbURL = nconf.get("DB_URL").replace("<dbuser>", dbUser).replace("<dbpassword>", dbPassword);
+const dbName = nconf.get("DB_NAME");
+
+// for the time being...
+let db;
+MongoClient.connect(dbURL)
+	.then(connection => connection.db(dbName))
+	.then(database => {		
+		console.log("Connected to DB");
+		db = database;
+	})
+	.catch(err => {
+		console.log(`Error: ${err}`);
+	});
 
 router.get("/", (req, res) => {
-	res.status(200).send("very data");
+	if (!db) {
+		res.status(500).send("Cannot connect to DB");
+	}
+	db.collection("test")
+		.find()
+		.toArray()
+		.then(result => {
+			let data = result[0].data;
+			res.status(200).send(data);
+		})
+		.catch(err => {
+			res.status(500).send(err);
+		});
 });
 
 module.exports = router;
