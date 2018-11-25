504212c61660c3b405dd6b0ca17d002ef856b3f1
cztamas <tamas.czank@gmail.com>
2018-11-25 22:23:51 +0100

    create webpack dev server

---
+++ webpack.dev.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/client/index.jsx",
+	devtool: "inline-source-map",
+	devServer: {
+		contentBase: "./dist/client/"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/, use: "babel-loader"
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(["dist"]),
+		new CopyWebpackPlugin([{
+			from: "**/*",
+			to: "..",
+			ignore: ["*.jsx"],
+			context: "src"
+		}])
+	],
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist", "client")
+	}
+};
---
+++ webpack.prod.config.js
@@ -0,0 +1,25 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: "./src/client/index.jsx",
+	output: {
+		path: path.resolve(__dirname, "dist", "client"),
+		filename: "index.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/, use: "babel-loader"
+			}
+		]
+	},
+	plugins: [
+		new CopyWebpackPlugin([{
+			from: "**/*",
+			to: "..",
+			ignore: ["*.jsx"],
+			context: "src"
+		}])
+	]
+};
